2bb972cc58460d4dc7f99623e45c10bc4eab2bcd
Rees, Nathan {MDBL~Welwyn} <nathan.rees@roche.com>
2022-06-30 15:46:08 +0200

    Created example of creating a function within a function

--- R/get_map.R
+++ R/get_map.R
@@ -1,1 +1,15 @@
 # get_map
+
+
+get_map <- function(val, val_fmt){
+  get_map_temp = function(input){
+    output = val
+    return(output)
+  }
+  return(get_map_temp)
+}
+
+get_map(val=c("USA", "CAN", "ENG"),
+        val_fmt=c("NA", "NA", "EUR"))
+
+
