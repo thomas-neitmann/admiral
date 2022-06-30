396712101aef847a00d6bfa351bac0a59ae67671
Rees, Nathan {MDBL~Welwyn} <nathan.rees@roche.com>
2022-06-30 16:08:23 +0200

    Added an example function with unlimitted arguements

--- R/get_map.R
+++ R/get_map.R
@@ -1,15 +1,29 @@
-# get_map
-
-
-get_map <- function(val, val_fmt){
-  get_map_temp = function(input){
+# Function within Function
+func <- function(val, val_fmt){
+  func_in_func = function(input){
     output = val
     return(output)
   }
-  return(get_map_temp)
+  return(func_in_func)
 }
 
-get_map(val=c("USA", "CAN", "ENG"),
+func(val=c("USA", "CAN", "ENG"),
         val_fmt=c("NA", "NA", "EUR"))
+
+
+# Function with unlimited arguments
+test <- function(...){
+  sum(...)
+}
+
+test(1,2,3)
+
+# get_map
+get_map <- function(...){
+  quote(...)
+  list(...)
+}
+
+get_map("Y" := 1, "N" := 0)
 
 
